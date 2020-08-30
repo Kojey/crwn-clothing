e792d82d1dd6321ea6dec4af96358ae3d375823f
Othniel Konan <othniel@elenjicalsolutions.com>
2020-08-30 11:06:32 +0200

    Update our menu-item to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -2,11 +2,18 @@ import React from 'react';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component'
+import { Route } from 'react-router-dom';
 
+const hats = () => (
+  <div>
+    <h1>HATS</h1>
+  </div>
+)
 function App() {
   return (
     <div>
-    <HomePage/>
+    <Route exact path='/' component={HomePage}/>
+    <Route path='/shop/hats' component={hats}/>
     </div>
   );
 }
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -31,7 +31,6 @@ class Directory extends React.Component {
           imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
           size: 'large',
           id: 4,
-          size: 'large',
           linkUrl: 'shop/womens'
         },
         {
@@ -39,7 +38,6 @@ class Directory extends React.Component {
           imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
           size: 'large',
           id: 5,
-          size: 'large',
           linkUrl: 'shop/mens'
         }
       ]
@@ -49,8 +47,8 @@ class Directory extends React.Component {
   render(){ 
     return(
       <div className="directory-menu">
-        {this.state.sections.map((props) =>
-          <MenuItem {...props} />)}
+        {this.state.sections.map(({id, ...props}) =>
+          <MenuItem key={id} {...props} />)}
       </div>
     );
   }
